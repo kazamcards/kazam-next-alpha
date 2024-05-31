d0c0c3489a8e3849605c9e4af513246edb38a0b1
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2024-05-31 14:15:25 +0100

    Formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,18 @@
-import Footer from "@/components/footer"
-import Header from "@/components/header"
+import Footer from "@/components/footer";
+import Header from "@/components/header";
 export default function Home() {
-
   return (
     <>
-    <Header/>
-    <main>
-<div class="border border-black bg-[#c907f7]">
-  <h1> Recently Added</h1>
-
-</div>
-<div class="border border-black bg-[#c907f7]">
-  <h1> Top Sellers </h1>
-</div>
-    </main>
-    <Footer/>
+      <Header />
+      <main>
+        <div className="border border-black bg-[#c907f7]">
+          <h1> Recently Added</h1>
+        </div>
+        <div className="border border-black bg-[#c907f7]">
+          <h1> Top Sellers </h1>
+        </div>
+      </main>
+      <Footer />
     </>
-  )}
+  );
+}

c89f68685f006ad7059cbe4bf14fd1c37f98f6cb
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2024-06-03 16:05:42 +0100

    Eras drop down added and functioning. CSS will be cleaned later (hovers)

--- src/app/lib/AllCategories.ts
+++ src/app/lib/AllCategories.ts
@@ -1,20 +1,6 @@
 import { db } from "./db";
 import { Product } from "../../components/types";
 
-// Get recently added products limited by last 10
-// export const queryCategories = `
-//     SELECT
-//     products.id, products.product_name, products.inventory, products.price, products.description, products.image_url,
-//     productscategory.category_name AS category,
-//     sets.set_name AS set,
-//     era.era_name AS era FROM products
-//   JOIN productscategory ON products.category_id = productscategory.id
-//   JOIN sets ON products.set_id = sets.id
-//   JOIN era ON sets.era_id = era.id
-//   ORDER BY products.id DESC
-//   LIMIT 10
-//     `;
-
 export const queryCategories = `
     SELECT * FROM productscategory
     `;

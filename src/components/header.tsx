5d658c5004458d34de268a57495144d9c8d45de4
Katya <katyasenior@gmail.com>
2024-06-03 09:11:23 +0100

    capitlised a single letter

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
         </div>
         <div className="flex flex-col items-center">
           <h1 className="text-3xl font-bold">Kazam Cards</h1>
-          <p>your one stop PKMN shop</p>
+          <p>Your one stop PKMN shop</p>
         </div>
       </header>
       <nav className="flex justify-evenly">

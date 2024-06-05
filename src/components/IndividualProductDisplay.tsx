489aab1c7892f181055a0a38d4f4e075dc2f21cb
Katya <katyasenior@gmail.com>
2024-06-05 14:10:10 +0100

    I rewrote some product page css and added a new component. However, I can't seem to get the product details to move in @media. Mobile is how we want it.

---
+++ src/components/IndividualProductDisplay.tsx
@@ -0,0 +1,62 @@
+// --- --- Main Product Display component --- ---
+// This image is temporary until we figure out individual product images:
+import TestPack from "../../public/images/test-pack.webp";
+import Image from "next/image";
+// Typing import:
+import { Product } from "./types";
+
+// This component will change what it displays depending on the Props it receives.
+// The 'fetchProducts' prop is a function call to the database, which is passed in from whichever parent is calling this component:
+export default async function ProductDisplay(props: {
+  fetchProducts: () => Promise<Product[]>;
+}) {
+  const response = await props.fetchProducts();
+  const productData: Product[] = response;
+
+  if (productData.length === 0) {
+    return <p>No products found.</p>;
+  }
+
+  return (
+    <>
+      {productData.map((product) => {
+        return (
+          <div
+            className="individual-product-card flex flex-col items-center border pt-1 rounded-xl bg-yellow-300"
+            key={product.id}
+          >
+            <div>
+              <h1>{product.product_name}</h1>
+            </div>
+            <Image src={TestPack} alt={"test image"} height={150} />
+
+            <p>£{product.price}</p>
+            {product.inventory > 0 ? (
+              <p>{product.inventory} in stock</p>
+            ) : (
+              <p className="bg-red-500 text-white text-center w-full">
+                {" "}
+                Sold out!{" "}
+              </p>
+            )}
+
+            {product.inventory > 0 ? (
+              <p className="bg-[var(--tertiary-coral-orange)]  text-center">
+                ADD TO BASKET BUTTON
+              </p>
+            ) : null}
+            <div className="description flex flex-col gap-2 justify-start w-full border-t border-b mt-1 mb-1 nset-y-0 right-0">
+              <h2>Product Details</h2>
+              <p>{product.era}</p>
+              <p>{product.set}</p>
+              <p>
+                This is a placeholder for the product details until we work out
+                how we&apos;re handling that.
+              </p>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+}

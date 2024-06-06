a10c22ae7cc221f12f8e9de22eddff997f9f9c97
isaacgomu <IsaacDalglish03@gmail.com>
2024-06-06 19:37:31 +0100

    needs styling

--- src/app/eras/page.jsx
+++ src/app/eras/page.jsx
@@ -4,20 +4,23 @@ import { slugify } from "../lib/slugify";
 
 export default async function ErasPage() {
   const eras = await allEras();
-  console.log(eras);
   return (
-    <>
+    <div className="flex flex-col items-center">
       <h2>Welcome to the Eras page!</h2>
-      <p>Eras are:</p>
-      {eras.map((era) => {
-        return (
-          <div key={era.id}>
-            <Link href={`/eras/${era.id}/${slugify(era.era_name)}`}>
+      <p>We&apos;ve got:</p>
+      <div className="categories-list flex flex-col justify-start gap-2 mt-4 mb-4 p-4 border">
+        {eras.map((era) => {
+          return (
+            <Link
+              key={era.id}
+              href={`/eras/${era.id}/${slugify(era.era_name)}`}
+              className="nav-link p-2 bg-[var(--secondary-uranian-blue)]"
+            >
               {era.era_name}
             </Link>
-          </div>
-        );
-      })}
-    </>
+          );
+        })}
+      </div>
+    </div>
   );
 }
